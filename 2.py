0c1b1400906bba3e088441b92bb02306b690cbc8
gux2hi <114000887+gux2hi@users.noreply.github.com>
2024-05-10 10:39:50 +0900

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+N = int(input())
+L = [(int(N) for N in input().split())]
+
+count = 0
+print(L)
+for i in range(L):
+    for j in range(L):
+        if(max(L(i))):
+            count += 1
+    print(count)
