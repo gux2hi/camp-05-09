88b809b5414fc6622582ec7c3000c7ddca10806a
gux2hi <114000887+gux2hi@users.noreply.github.com>
2024-05-13 11:04:44 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,28 @@
-N = int(input())
-L = [(int(N) for N in input().split())]
+N,L = map(int, input().split())
+scores = [] # 学生、試験ごとのスコアを格納する2次元配列
 
-count = 0
-print(L)
 for i in range(L):
+    student = list(map(int, input().split()))
+    scores.append(student)
+
+tops = {}
+for i in range(N):
+    top_score = 0
+    top_student = 0
     for j in range(L):
-        if(max(L(i))):
-            count += 1
-    print(count)
+        if( top_score < scores[j][i]):
+            top_score = scores[j][i]
+            top_student = j
+    if(top_student + 1 in tops):
+        tops[top_student + 1] += 1
+    else:
+        tops[top_student + 1] = 1
+
+
+max_val = max(tops.values())
+min_student = L
+for i in tops.keys():
+    if(tops[i] == max_val and i < L ):
+        min_student = i
+
+print(f'{min_student} {max_val}')
